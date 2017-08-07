e1356d96597f8ca9bb1235cb47345c4f37a46564
unknown <fernandoapeguero@gmail.com>
2017-08-06 22:52:45 -0400

    added ContentProvider Java class Need to add the CursorAdapter to display the data and the ListView to the main View

--- app/src/main/java/com/fernandoapeguero/myinventory/data/InventoryContract.java
+++ app/src/main/java/com/fernandoapeguero/myinventory/data/InventoryContract.java
@@ -1,5 +1,7 @@
 package com.fernandoapeguero.myinventory.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,6 +10,10 @@ import android.provider.BaseColumns;
 
 public class InventoryContract  {
 
+    public static final String CONTENT_AUTHORITY = "com.inventoryprovider";
+    public static final String PATH_INVENTORY = "inventory";
+    public static final Uri BASECONTENT_URI = Uri.parse("Content://" + CONTENT_AUTHORITY);
+
     public static final class InventoryEntrys implements BaseColumns{
 
         public static final String TABLE_NAME = "inventory";
@@ -15,6 +21,23 @@ public class InventoryContract  {
         public static final String PRODUCT_PRICE = "price";
         public static final String PRODUCT_QUANTITY = "quantity";
         public static final String PRODUCT_WEIGHT = "weight";
+
+        public static  final Uri CONTENT_URI = Uri.withAppendedPath(BASECONTENT_URI , PATH_INVENTORY);
+
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         *  The MIME type of the {@link #CONTENT_URI} for a single product.
+         */
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE+ "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
 
 
     }
--- app/src/main/java/com/fernandoapeguero/myinventory/data/InventoryProvider.java
+++ app/src/main/java/com/fernandoapeguero/myinventory/data/InventoryProvider.java
@@ -1,47 +1,166 @@
 package com.fernandoapeguero.myinventory.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.fernandoapeguero.myinventory.data.InventoryContract.InventoryEntrys;
 
 /**
  * Created by nico on 8/4/2017.
  */
 
 public class InventoryProvider extends ContentProvider {
+    private static final String LOG_TAG = InventoryProvider.class.getName();
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    private InventoryDbHelper iDbHelper;
+
+    private static final int PRODUCT = 1000;
+    private static final int PRODUCT_ID = 1001;
+
     @Override
     public boolean onCreate() {
-        return false;
+
+        iDbHelper = new InventoryDbHelper(getContext());
+        return true;
+    }
+
+    static {
+          // SETTING THE URI FOR ALL PRODUCTS AND ADDING IT TO THE URIMATCHER
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY,PRODUCT);
+
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY + "/#" , PRODUCT_ID);
+
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+
+        SQLiteDatabase db = iDbHelper.getReadableDatabase();
+
+        Cursor cursor = null;
+
+        int match = sUriMatcher.match(uri);
+        switch (match){
+            case PRODUCT:
+
+               cursor = db.query(InventoryEntrys.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
+                break;
+            case PRODUCT_ID:
+
+                selection = InventoryEntrys._ID + " = ?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                cursor = db.query(InventoryEntrys.TABLE_NAME,projection,selection,selectionArgs,null,null,sortOrder);
+                break;
+            default:
+               throw  new IllegalArgumentException("Cursor can't query unknow uri" + uri);
+
+
+
+        }
+        return cursor;
     }
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+
+        int match = sUriMatcher.match(uri);
+
+        switch (match){
+            case PRODUCT:
+               return InventoryEntrys.CONTENT_LIST_TYPE;
+            case PRODUCT_ID:
+                return InventoryEntrys.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri + " with match " + match);
+
+        }
+
     }
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+
+        int match = sUriMatcher.match(uri);
+        switch (match){
+            case PRODUCT:
+                // insert method for inserting the values into the database
+                saveInventory(uri,values);
+                break;
+            default:
+                throw new IllegalArgumentException("insertion is not supported for " + uri);
+        }
+
         return null;
+    }
+
+    private Uri saveInventory(Uri uri,ContentValues values){
+
+
+
+        SQLiteDatabase db = iDbHelper.getWritableDatabase();
+
+       long id =  db.insert(InventoryEntrys.TABLE_NAME,null,values);
+
+         if (id == -1 ){
+
+             Log.e(LOG_TAG, "failed to insert row for " + uri);
+         }
+     return ContentUris.withAppendedId(uri,id);
     }
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+
+        SQLiteDatabase db = iDbHelper.getWritableDatabase();
+
+        int deletedRows;
+
+
+        int match = sUriMatcher.match(uri);
+
+        switch (match){
+            case PRODUCT:
+
+               deletedRows = db.delete(InventoryEntrys.TABLE_NAME,selection,selectionArgs);
+                break;
+            case PRODUCT_ID:
+
+                selection = InventoryEntrys._ID + " = ?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                deletedRows = db.delete(InventoryEntrys.TABLE_NAME,selection,selectionArgs);
+               break;
+                default:
+                    throw new IllegalArgumentException("can not match uri " + uri);
+        }
+
+
+
+        return deletedRows;
     }
 
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+
+        SQLiteDatabase db = iDbHelper.getWritableDatabase();
+
+       int updatedRows =  db.update(InventoryEntrys.TABLE_NAME,values,selection,selectionArgs);
+
+
+        return updatedRows;
     }
 }
